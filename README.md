1c165d5bf0fbc32f3d9c4f5d265bfdfa9802d694
LuanBricio <luanbricio0@gmail.com>
2025-02-18 18:07:42 -0300

    comecei o desenvolvimento, coloquei a imagem e o Header

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Estou criando uma nova versão do meu portfólio, usando React
 
 # Resultado de como deve ficar o design
 <img src="./imgs/imagem para o portfólio.jpg">
+(imagem gerada com IA)
---
+++ projeto-portfolio/src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+export default function Header(){
+    return(
+        <div className="Header">
+            <ul>
+                <li>Home</li>
+                <li>Portfólio</li>
+                <li>Contato</li>
+            </ul>
+        </div>
+    )
+}
